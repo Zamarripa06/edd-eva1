edf1405e2d98ae73e40a055ff11c5a69b19b51ee
Zamarripa06 <danielzamarron2f@gmail.com>
2026-08-31 16:56:54 -0600

    Add files via upload

---
+++ Evav_1/src/Evav_1_SCOOPE.java
@@ -0,0 +1,10 @@
+public class Evav_1_SCOOPE{
+    public static void main(String[] args){
+        int  x = 100;
+        for(int i=0; i<10; i++) {
+        x++;
+        }
+        System.out.println("Valor final de la i = " + i);
+        System.out.println("Valor final de la x = " + x);
+    }
+}
